1004a614fbf7089f86a8db9c3796043edb686b13
qizheng.wen <qizheng.wen@yeepay.com>
2019-10-22 18:12:37 +0800

    no message

--- Algorithm/Algorithm/Sort.swift
+++ Algorithm/Algorithm/Sort.swift
@@ -10,36 +10,19 @@ import Foundation
 
 public class Sort {
     //基础冒泡
-    public class func bubbleSort(nums:[Int]){
-        var sorted = nums;
-        for i in 0..<sorted.count {
-            for j in 0..<(sorted.count-i-1){
-                if sorted[j] > sorted[j + 1] {
-                    sorted.swapAt(j, j+1);
+    public class func bubbleSort(nums:inout [Int]) -> [Int]{
+        for i in 0..<nums.count - 1  {
+            for j in 0..<nums.count - i - 1{
+                if nums[j] > nums[j+1] {
+                    nums.swapAt(j, j+1);
                 }
             }
         }
-        print(sorted);
-    }
-    //优化冒泡1：若未发生交换，则说明已是有序
-    public class func bubbleSort1(nums:[Int]){
-        var sorted = nums;
-        for i in 0..<sorted.count {
-            var isSorted = false;
-            if sorted[i] < sorted[i+1] {continue};
-            for j in 0..<(sorted.count-i-1){
-                if sorted[j] > sorted[j + 1] {
-                    sorted.swapAt(j, j+1);
-                    isSorted = true;
-                }
-            }
-            if isSorted {break;}
-        }
-        print(sorted);
+        return nums;
     }
     
-    //优化冒泡2：若某位置之后未发生交换，则说明该位置后已是有序
-    public class func bubbleSort2(nums:[Int]){
+    //优化冒泡1：若某位置之后未发生交换，则说明该位置后已是有序
+    public class func bubbleSort1(nums:[Int]){
         var sorted = nums;
         
         var last = nums.count-1;
@@ -58,8 +41,8 @@ public class Sort {
         print(sorted);
     }
     
-    //优化冒泡3：若某位置之后未发生交换，则说明该位置后已是有序
-    public class func bubbleSort3(nums:[Int]){
+    //优化冒泡2：若某位置之后未发生交换，则说明该位置后已是有序
+    public class func bubbleSort2(nums:[Int]){
         var sorted = nums;
         
         var start = 0;
@@ -94,39 +77,111 @@ public class Sort {
         print(sorted);
     }
     
-    class func insertSort(nums:[Int]) {
-        var sorted = nums;
-        
-        for i in 1..<sorted.count {
-            var preIndex = i - 1;
-            let current = sorted[i];
-            
-            while (preIndex >= 0) && (sorted[preIndex] > current) {
-                sorted[preIndex + 1] = sorted[preIndex];
-                preIndex -= 1;
-            }
-            sorted[preIndex + 1] = current;
-        }
-        
-        print(sorted);
-    }
-    
-    class func selectSort(nums:[Int]){
-        var sorted = nums;
-        
-        for i in 0..<sorted.count {
+    public class func selectSort(nums:inout [Int]){
+        for i in 0..<nums.count - 1 {
             var minIndex = i;
-            
-            var isSorted = true;
-            for j in (i+1)..<sorted.count {
-                if sorted[j] < sorted[minIndex] {
+            for j in i+1..<nums.count {
+                if nums[j] < nums[minIndex] {
                     minIndex = j;
-                    isSorted = false;
                 }
             }
-            if isSorted { break;}
-            
-            sorted.swapAt(i, minIndex);
+            nums.swapAt(minIndex, i);
         }
     }
+    
+    public class func insertSort(nums:inout [Int]) {
+        for i in 1..<nums.count {
+            for j in (1...i).reversed() {
+                if nums[j] < nums[j-1] {
+                    nums.swapAt(j, j-1)
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+    
+    //优化，通过对比从后向前赋值，减少交换
+    public class func insertSort1(nums:inout [Int]) {
+        for i in 1..<nums.count {
+            let current = nums[i];
+            var j = i;
+            while j > 0 {
+                if current < nums[j-1] {
+                    nums[j] = nums[j-1];
+                    j -= 1;
+                }
+            }
+            nums[j] = current;
+        }
+    }
+    
+    public class func quickSort(nums: inout [Int], left: Int,  right: Int){
+        //递归出口
+        if left >= right{
+            return;
+        }
+        
+        var l = left;
+        var r = right;
+        let p = nums[l];
+
+        while l < r {
+            while (left < right) && nums[right] >= p {
+                r -= 1;
+            }
+            nums[l] = nums[r];
+            while (l < r) && nums[l] <= p {
+                l += 1;
+            }
+            nums[r] = nums[l];
+        }
+        nums[l] = p;
+        
+        quickSort(nums: &nums, left: left, right: l-1);
+        quickSort(nums: &nums, left: l+1, right: right);
+    }
+    
+    func mergeArray(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        
+        var p = m + n - 1;
+        var p1 = m;
+        var p2 = n;
+        
+        while p1 >= 0 && p2 >= 0 {
+            if nums1[p1] > nums2[p2] {
+                nums1[p] = nums1[p1];
+                p1 -= 1;
+            }else{
+                nums1[p] = nums2[p2];
+                p2 -= 1;
+            }
+            p -= 1;
+        }
+        
+        while p2 >= 0 {
+            nums1[p] = nums2[p2];
+            p2 -= 1;
+            p -= 1;
+        }
+    }
+    
+//    public class func mergeSort(nums:inout [Int]){
+//        var size = 2;
+//        while size <= nums.count + (nums.count%2 != 0 ? 1:0) {
+//            let partCount = nums.count/size + (nums.count%size != 0 ? 1:0);
+//            for i in 0..<partCount/2 {
+//                _mergeSort(nums: &nums, f: i, s: i+size);
+//            }
+//            size = size * 2;
+//        }
+//    }
+    
+//    class func _mergeSort(nums: inout [Int], f:Int, s:Int){
+//        var temp = nums.range;
+//
+//        for i in i..<(i+l) {
+//
+//        }
+//    }
 }
