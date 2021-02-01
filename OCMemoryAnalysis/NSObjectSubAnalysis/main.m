7b5725f85fd41a84269901e6aff80d54c1154754
qizheng.wen <qizheng.wen@yeepay.com>
2021-02-01 19:31:21 +0800

    no message

---
+++ OCMemoryAnalysis/NSObjectSubAnalysis/main.m
@@ -0,0 +1,36 @@
+//
+//  main.m
+//  Analysis2
+//
+//  Created by yp-tc-m-2548 on 2021/2/1.
+//  Copyright © 2021 KiraLayo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+#import <malloc/malloc.h>
+
+@interface NSObjectSub : NSObject
+
+@property NSString *name;
+
+@end
+
+@implementation NSObjectSub
+
+@end
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSObjectSub *obj = [[NSObjectSub alloc] init];
+
+        // 获得NSObject实例对象的成员变量所占用的大小 >> 8
+        NSLog(@"%zd", class_getInstanceSize([NSObjectSub class]));
+
+        // 获得obj指针所指向内存的大小 >> 16
+        NSLog(@"%zu", malloc_size((__bridge const void *)obj));
+
+    }
+    return 0;
+}
